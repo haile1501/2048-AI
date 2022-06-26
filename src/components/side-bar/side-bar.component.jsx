88fa1b71a2ea491bd482fcbc5c60a8d35da8af30
Haibeo <h0000151515@gmail.com>
2022-06-26 10:44:17 +0700

    input validation

--- src/components/side-bar/side-bar.component.jsx
+++ src/components/side-bar/side-bar.component.jsx
@@ -4,6 +4,7 @@ import Button from '../button/button.component';
 import { AiContext } from '../../context/ai.context';
 import { GameStateContext } from '../../context/game-state.context';
 import OptionsBar from '../options-bar/options-bar.component';
+import ErrorMessage from '../error-message/error-massage.component';
 
 import './side-bar.styles.scss';
 import { useState } from 'react';
@@ -58,11 +59,21 @@ const SideBar = () => {
     const applyChange = () => {
         if (gameOver) {
             setChangeWhenOver(true);
-            setTrial(+config.numberOfPlays);
+            if (+config.numberOfPlays > 0) {
+                setTrial(+config.numberOfPlays);
+            }
         }
-        setNumberOfIterations(+config.numberOfIterations);
-        setSimulationDepth(+config.simulationDepth);
-        setCount(+config.numberOfPlays);
+        if (Number.isInteger(+config.numberOfIterations) && +config.numberOfIterations > 1) {
+            setNumberOfIterations(+config.numberOfIterations);
+        }
+        
+        if  (Number.isInteger(+config.simulationDepth) && +config.simulationDepth > 0) {
+            setSimulationDepth(+config.simulationDepth);
+        }
+        
+        if (Number.isInteger(+config.numberOfPlays) && +config.numberOfPlays > 0) {
+            setCount(+config.numberOfPlays);
+        }
     }
 
     return (
@@ -89,14 +100,17 @@ const SideBar = () => {
                         </div>
                         :
                         <div className='mcts-config'>
-                            <label htmlFor="numberOfIterations">Number of iterations</label>
+                            <label htmlFor="numberOfIterations">Number of iterations: {numberOfIterations}</label>
+                            <ErrorMessage>{(Number.isInteger(+config.numberOfIterations) && +config.numberOfIterations > 1) ? '' : 'Integer greater than 1 required!'}</ErrorMessage>
                             <input type="text" name="numberOfIterations" id="numberOfIterations" value={config.numberOfIterations} onChange={event => handleInputChange(event)} />
-                            <label htmlFor="simulationDepth">Simulation depth</label>
+                            <label htmlFor="simulationDepth">Simulation depth: {simulationDepth}</label>
+                            <ErrorMessage>{(Number.isInteger(+config.simulationDepth) && +config.simulationDepth > 0) ? '' : 'Positive integer required!'}</ErrorMessage>
                             <input type="text" name="simulationDepth" id="simulationDepth" value={config.simulationDepth} onChange={event => handleInputChange(event)} />
                         </div>
                 }
                 <div className='footer'>
-                    <label htmlFor="numberOfPlays">Times</label>
+                    <label htmlFor="numberOfPlays">Times: {count}</label>
+                    <ErrorMessage>{(Number.isInteger(+config.numberOfPlays) && +config.numberOfPlays > 0) ? '' : 'Positive integer required!'}</ErrorMessage>
                     <input type="text" name="numberOfPlays" value={config.numberOfPlays} onChange={event => handleInputChange(event)} id="numberOfPlays" />
                     <Button onClick={applyChange}>Apply</Button>
                 </div>
